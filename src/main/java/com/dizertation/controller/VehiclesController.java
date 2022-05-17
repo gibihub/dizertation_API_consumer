8fbdee6d69f326db9ee9ba2a9a88279341a23221
gibihub <33116091+gibihub@users.noreply.github.com>
2022-05-17 19:45:45 +0300

    add request param

--- src/main/java/com/dizertation/controller/VehiclesController.java
+++ src/main/java/com/dizertation/controller/VehiclesController.java
@@ -5,6 +5,7 @@ import com.dizertation.model.Vehicles;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,12 +19,12 @@ public class VehiclesController {
     @Autowired
     private RestTemplate restTemplate;
     
-    @GetMapping("/vehicles?r=r1551&format=json")
-    public List<Vehicle> getVehicles() {
+    @GetMapping("/vehicles")
+    public List<Vehicle> getVehiclesById(@RequestParam(value = "r") String id) {
         String url = "https://rt.api.opentransport.ro/api/v1/key/f78a2e9a/agency/ro.stpt/command/vehicles?r=r1551&format=json";
         Vehicle[] vehicles= restTemplate.getForObject(url, Vehicle[].class);
         List<Vehicle> vehiclesList = Arrays.asList(vehicles);
-        System.out.println("Bus id : " +vehiclesList.get(0).getId());
+        System.out.println("Bus id : " + vehiclesList.get(0).getId());
         return vehiclesList;
     }
 
