cc7029ac927370c5ec11732f251013bd2ec447f3
rgiblak <raul.giblak98@e-uvt.ro>
2022-05-19 12:03:34 +0300

    Try new rest

--- src/main/java/com/dizertation/controller/VehiclesController.java
+++ src/main/java/com/dizertation/controller/VehiclesController.java
@@ -2,7 +2,9 @@ package com.dizertation.controller;
 
 import com.dizertation.model.Vehicle;
 import com.dizertation.model.Vehicles;
+import com.dizertation.service.ParsingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,19 +15,22 @@ import java.util.Arrays;
 import java.util.List;
 
 @RestController
-@RequestMapping("/myVehicles")
 public class VehiclesController {
 
+    private static final String MAIN_PAGE =
+            "https://rt.api.opentransport.ro/web/reports/apiCalls/vehiclesApiParams.jsp?a=ro.stpt";
+
+    private static final String E2_PAGE_JSON =
+            "https://rt.api.opentransport.ro/api/v1/key/f78a2e9a/agency/ro.stpt/command/vehicles?r=r1551&format=json";
+
     @Autowired
-    private RestTemplate restTemplate;
-    
-    @GetMapping("/vehicles")
-    public List<Vehicle> getVehiclesById(@RequestParam(value = "r") String id) {
-        String url = "https://rt.api.opentransport.ro/api/v1/key/f78a2e9a/agency/ro.stpt/command/";
-        Vehicle[] vehicles= restTemplate.getForObject(url, Vehicle[].class);
-        List<Vehicle> vehiclesList = Arrays.asList(vehicles);
-        System.out.println("Bus id : " + vehiclesList.get(0).getId());
-        return vehiclesList;
+    private ParsingService parsingService;
+
+    @GetMapping
+    public String main(final Model model) {
+        List<Vehicle> vehicleList = (List<Vehicle>)parsingService.parse(E2_PAGE_JSON);
+        model.addAttribute("vehicle", vehicleList.get(0));
+        return MAIN_PAGE;
     }
 
 }
